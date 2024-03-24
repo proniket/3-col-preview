c3527a7c5bdbe26db146ba94066f78b394b060f5
proniket <proniket01@gmail.com>
2024-03-24 21:24:58 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				pumpkin: '#e28525 ',
+				teal: '#016972',
+				'deep-jungle-green': '#00403f',
+			},
+		},
+	},
+	plugins: [],
+};
+
